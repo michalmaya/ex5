f5ae9e4ffbd57728e2eba5ad3ea87beae932119c
michalmaya <michal.maya75@gmail.com>
2022-06-28 16:52:21 +0300

    merged

--- ex5.py
+++ ex5.py
@@ -6,19 +6,23 @@ from collections import defaultdict
 def names_of_registered_students(input_json_path, course_name):
     with open(input_json_path, 'r') as f:
         tempDict = json.load(f)
-    return [tempDict["student_name"] for tempDict["student_name"] in tempDict if
+    return [name for name in tempDict["student_name"] if
             course_name in tempDict["registered_courses"]]
 
 
-def enrollment_numbers(input_json_path, output_file_path):
-    """
-    This function writes all the course names and the number of enrolled
-    student in ascending order to the output file in the given path.
 
-    :param input_json_path: Path of the students database json file.
-    :param output_file_path: Path of the output text file.
-    """
-    pass
+def enrollment_numbers(input_json_path, output_file_path):
+    with open(input_json_path, 'r') as fileIn:
+        tempDict = json.load(fileIn)
+    coursesList = [course for course in tempDict.value]
+    coursesList.sort()
+    coursesDict = {}
+    for course in coursesList:
+        if course not in coursesDict.keys():
+            coursesDict[course]= len(names_of_registered_students(input_json_path, course))
+    with open(output_file_path, 'w') as fileOut:
+        json.dump(coursesDict, fileOut, indent=4)
+
 
 
 def courses_for_lecturers(json_directory_path, output_json_path):
