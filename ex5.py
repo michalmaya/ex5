dcb0afa123a6bf9ef737da8eb47b49b29fc02e8b
shayro9 <shayro13@gmail.com>
2022-06-28 15:37:26 +0300

    last function

--- ex5.py
+++ ex5.py
@@ -1,16 +1,13 @@
 import json
 import os
-
+from collections import defaultdict
 
 
 def names_of_registered_students(input_json_path, course_name):
-    with open (input_json_path, 'r') as f:
+    with open(input_json_path, 'r') as f:
         tempDict = json.load(f)
-    return [tempDict["student_name"] for tempDict["student_name"] in tempDict if course_name in tempDict["registered_courses"]]
-
-
-
-
+    return [tempDict["student_name"] for tempDict["student_name"] in tempDict if
+            course_name in tempDict["registered_courses"]]
 
 
 def enrollment_numbers(input_json_path, output_file_path):
@@ -24,7 +21,6 @@ def enrollment_numbers(input_json_path, output_file_path):
     pass
 
 
-
 def courses_for_lecturers(json_directory_path, output_json_path):
     """
     This function writes the courses given by each lecturer in json format.
@@ -32,7 +28,16 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     :param json_directory_path: Path of the semsters_data files.
     :param output_json_path: Path of the output json file.
     """
-    pass
-
-
-
+    fileList = os.listdir(json_directory_path)
+    finalDict = defaultdict(list)
+    for i in fileList:
+        if i.endswith(".json"):
+            with open(json_directory_path + "/" + i, 'r') as f:
+                temp_dict = json.load(f)
+                for item in temp_dict:
+                    for lecturer in temp_dict[item]["lecturers"]:
+                        course = temp_dict[item]["course_name"]
+                        if course not in finalDict[lecturer]:
+                            finalDict[lecturer] += [course]
+    with open(output_json_path, 'w') as f:
+        json.dump(finalDict, f, indent=4)
---
+++ minitest.py
@@ -0,0 +1,5 @@
+from ex5 import names_of_registered_students, enrollment_numbers, courses_for_lecturers
+import json
+
+if __name__ == '__main__':
+    courses_for_lecturers("./semesters_databases", "./semesters_databases/trash.txt" )
